f566bb28b960129a03be26db5c654e624a5a3467
Chris Dorner <chris5011@chris5011.dev>
2022-10-25 18:47:10 +0200

    Fixed indentation problem

--- demoAPI.py
+++ demoAPI.py
@@ -16,7 +16,7 @@ def index():
 		"2.8.0":"{0}".format(str(random.randint(20000,35000))),
 		"1.7.0":"{0}".format(str(random.randint(100,1000))),
 		"2.7.0":"{0}".format(str(random.randint(100,1000))),
-        "32.7.0":"{0}".format(str(random.randint(1000,5500))),
+        	"32.7.0":"{0}".format(str(random.randint(1000,5500))),
 		"52.7.0":"{0}".format(str(random.randint(1000,5500))),
 		"72.7.0":"{0}".format(str(random.randint(1000,5500))),
 		"31.7.0":"{0}".format(str(random.randint(10,250))),
@@ -28,17 +28,3 @@ def index():
 	return jsonData
 
 app.run()
-
-
-#   	"1.8.0":"10912315",
-#		"2.8.0":"1851416",
-#		"1.7.0":"560",
-#		"2.7.0":"450",
-#		"32.7.0":"2347",
-#		"52.7.0":"2353",
-#		"72.7.0":"2356",
-#		"31.7.0":"86",
-#		"51.7.0":"263",
-#		"71.7.0":"133",
-#		"13.7.0":"321",
-#		"uptime":"0000:21:57:17"
