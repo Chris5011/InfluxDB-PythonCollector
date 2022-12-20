ddcfa61a35b10b33404229ce2d92bb6de6200365
Chris5011 <chris5011@chris5011.dev>
2022-12-20 09:50:01 +0100

    removed the startCollector.sh.bak

--- startCollector.sh
+++ startCollector.sh
@@ -4,5 +4,5 @@
 #Desc: This script is used to start the DataCollector (Used for the Service as starter). 
 
 echo "Starting the Data-Collector..."
-/usr/bin/python3 ./EVN-InfluxDB-DataCollector.py 
+/usr/bin/python3 <INSTALLPATH>/EVN-InfluxDB-DataCollector.py 
 
